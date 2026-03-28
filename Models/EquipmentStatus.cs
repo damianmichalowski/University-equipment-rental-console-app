ceeb2c730947dac02fda4217960b21e42a15a663
Damian Michałowski <damianmichalowskidm@gmail.com>
2026-03-28 15:13:06 +0100

    init project structure and test first classes

---
+++ Models/EquipmentStatus.cs
@@ -0,0 +1,8 @@
+namespace University_equipment_rental_console_app.Models;
+
+public enum EquipmentStatus
+{
+    Available,
+    Rented,
+    Unavailable
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using University_equipment_rental_console_app.Models;
+
+
+var equipment = new Laptop(1, "Test sprzęt");
+Console.WriteLine(equipment);
